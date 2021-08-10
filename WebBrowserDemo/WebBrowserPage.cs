16bbae4606eb844fcfd2ea8af579daedbb2ca8c0
小白 <2806933146@qq.com>
2021-08-10 10:17:45 +0800

    提交WebBrowserDemo

---
+++ WebBrowserDemo/WebBrowserPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JointdevOfWinFormAndWebDemo
+{
+    public partial class WebBrowserPage : Form
+    {
+        public WebBrowserPage()
+        {
+            InitializeComponent();
+        }
+
+        private void WebBrowserPage_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.baidu.com/");
+        }
+    }
+}
